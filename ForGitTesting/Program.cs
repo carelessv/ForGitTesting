d19441264feb18d18878cfc1369f4a69a66c0750
Salahuddin.Farooqui <Salahuddin.Farooqui@DSI001C1191728>
2017-07-05 15:55:28 +0530

    Add project files.

---
+++ ForGitTesting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForGitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From Salahuddin and Varun");
+        }
+    }
+}
